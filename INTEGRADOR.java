fdbff9c8a315250c31fa0154b355ee3a8cf02307
Fernando Montaño <140466094+ALEXANDER5236@users.noreply.github.com>
2023-10-30 07:43:00 -0500

    Add files via upload

---
+++ INTEGRADOR.java
@@ -0,0 +1,34 @@
+﻿import java.util.*;
+import java.lang.Math;
+
+class Program {
+
+    public static void main(String[] args) {
+        int teclado;
+        double sueldo;
+
+        sueldo = 0;
+        int categoria;
+
+        System.out.println("ingresar el tipo de categoría ");
+
+        Scanner teclado =  Scanner(System.in);
+        categoria = teclado;
+        if (categoria == 1) {
+            sueldo = 500 + 500 * 0.1;
+        } else {
+            if (categoria == 2) {
+                sueldo = 450 + 450 * 0.1;
+            } else {
+                if (categoria == 3) {
+                    sueldo = 600 + 600 * 0.1;
+                } else {
+                    System.out.println("ingresar el número establecido para categorias");
+                }
+            }
+        }
+        if (categoria == 1 || categoria == 2 || categoria == 3) {
+            System.out.println("El total del sueldo de la categoria " + categoria + " es igual a " + sueldo);
+        }
+    }
+}
